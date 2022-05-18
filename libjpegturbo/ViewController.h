85f98248ad3512701b4f4a0dc43cbeea9afec871
chenzijie <chenzijie@weidian.com>
2022-05-18 23:27:46 +0800

    Initial Commit

---
+++ libjpegturbo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  libjpegturbo
+//
+//  Created by blackox626 on 2022/5/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ libjpegturbo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  libjpegturbo
+//
+//  Created by blackox626 on 2022/5/18.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
